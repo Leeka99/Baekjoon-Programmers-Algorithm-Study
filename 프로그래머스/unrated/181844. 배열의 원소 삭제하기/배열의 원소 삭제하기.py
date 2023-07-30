084c1f6e4ee37979645313675eb723bcfc9e6583
DSRN <139203708+DSRN0000@users.noreply.github.com>
2023-07-30 22:25:54 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -0,0 +1,10 @@
+def solution(arr, delete_list):
+    answer = []
+    s1 = []
+    for i in arr:
+        for j in delete_list:
+            if i == j:
+                s1.append(i)
+                arr = [x for x in arr if x not in s1 ]
+    answer = arr
+    return answer
