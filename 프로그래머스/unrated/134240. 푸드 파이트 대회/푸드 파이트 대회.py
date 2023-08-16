0d9f2dd529d8a2a8aa29ac1fa0d19260cbed1fea
Do_Something! <139203708+DSRN0000@users.noreply.github.com>
2023-08-16 16:12:26 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,8 +1,5 @@
 def solution(food):
     answer = ''
     for i in range(1,len(food)):
-        if food[i] % 2 != 0:
-            n = i-1
-        for j in range(food[i]//2):
-            answer +=str(i)
+        answer += str(i)*(food[i]//2)
     return answer+'0'+''.join(list(reversed(answer)))
