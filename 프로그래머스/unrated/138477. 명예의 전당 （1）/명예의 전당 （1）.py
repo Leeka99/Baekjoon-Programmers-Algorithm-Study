53ef1fdcdb4a23c1ebbe9a498ebf1af0f488cd29
Do_Something! <139203708+DSRN0000@users.noreply.github.com>
2023-08-18 18:08:58 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,15 @@
+def solution(k, score):
+    answer = []
+    result = []
+    for i in range(len(score)):
+        if i<=k-1:
+            result.append(score[i])
+            answer.append(min(result))
+            #print(answer.append((min(result))))
+        else:
+            if min(result)<score[i]:
+                result[result.index(min(result))] = score[i]
+                answer.append(min(result))
+            else:
+                answer.append(min(result))
+    return answer
