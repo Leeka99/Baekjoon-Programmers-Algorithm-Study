ede1c77932f3f4ccc35c43c3920d5b1c10d2950e
Do_Something! <139203708+DSRN0000@users.noreply.github.com>
2023-10-29 01:19:08 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,12 @@
+subject_score = {"A+":4.5, "A0":4.0, "B+":3.5, "B0":3.0, "C+":2.5, "C0":2.0, "D+":1.5, "D0":1.0, "F":0.0}
+answer = 0
+subject_num = 0
+mul = 0
+for _ in range(20):
+    name,num,score = input().split()
+    if score == "P":
+        continue
+    subject_num += float(num)
+    mul += float(num) * subject_score[score]
+answer = mul/subject_num
+print(answer)
