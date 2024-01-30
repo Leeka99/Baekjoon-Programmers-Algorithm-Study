961dcf26e2458ad71f4e8410c39371d081c5dfe9
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-01-30 13:35:37 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,12 +1,14 @@
 import sys
-from collections import deque
-N, k = map(int, sys.stdin.readline().split())
-queue = deque(range(1, N+1))
 
-pus = list()
-while len(queue) != 0:
-    for _ in range(k-1):
-        queue.append(queue.popleft())
-    pus.append(str(queue.popleft()))
+n, k = map(int, sys.stdin.readline().split())
 
-print('<'+', '.join(pus)+'>')
+idx = 0
+queue = [i for i in range(1, n+1)]
+res = []
+while queue:
+    idx += k - 1  # k-1번째 인덱스까지 건너뛰기
+    if idx >= len(queue):  # 인덱스가 범위를 넘어갈 경우
+        idx %= len(queue)  # 나머지 연산을 통해 인덱스 계산
+    res.append(str(queue.pop(idx)))  # k번째 수 제거 후 결과 배열에 추가
+
+print("<", ", ".join(res), ">", sep="")
