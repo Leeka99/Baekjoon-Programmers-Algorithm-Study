c41e0ee8472648cd0a468e69c14d2806c97b5534
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2025-10-11 10:02:31 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+        int number = n % 2;
 
-        if (n % 2 == 0) System.out.println("CY");
-        if (n % 2 != 0) System.out.println("SK");
+        if (number == 0) System.out.println("CY");
+        if (number != 0) System.out.println("SK");
     }
 }
