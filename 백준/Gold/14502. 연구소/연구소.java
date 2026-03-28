11000161be834850c135cafe5e88da9f4b928ef3
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-03-28 12:09:54 +0900

    [Gold IV] Title: 연구소, Time: 1032 ms, Memory: 303580 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,102 +1,123 @@
 import java.util.*;
-import java.io.*;
-
-class Point {
-    int x, y;
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
 
 public class Main {
-    public static int n, m;
-    public static int[][] map;
-    public static int answer = Integer.MIN_VALUE;
-    public static int[] dx = new int[]{1, 0, -1, 0};
-    public static int[] dy = new int[]{0, 1, 0, -1};
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n + 1][m + 1];
+    private static int n, m, answer = 0;
+    private static int[][] map;
+    private static int[] dx = { 0, 1, 0, -1 }; // 가로
+    private static int[] dy = { -1, 0, 1, 0 }; // 세로
 
-        // 입력받기
+    private static void test() {
+        System.out.println();
         for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= n; j++) {
+                System.out.print(map[i][j] + " ");
             }
-        }
-
-        // 벽 세우기 시작
-        buildWall(0);
-
-        // 정답 출력
-        System.out.println(answer);
-
-    }
-
-    // 벽 세우기 : DFS
-    public static void buildWall(int cnt) {
-        
-        // 벽 3개 세웠다면 바이러스 퍼트리기 시작
-        if (cnt == 3) {
-            spreadVirus();
-            return;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (map[i][j] == 0) {
-                    map[i][j] = 1;
-                    buildWall(cnt + 1);
-                    map[i][j] = 0; // 백 트래킹
-                }
-            }
+            System.out.println();
         }
     }
 
-    // 바이러스 퍼트리기 : BFS
-    public static void spreadVirus() {
-        Queue<Point> queue = new LinkedList<>();
-        int[][] copy = new int[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) copy[i] = map[i].clone();
+    private static boolean canGo(int nx, int ny, int[][] copy) {
+        if (1 > nx || 1 > ny || nx > n || ny > m)
+            return false;
 
-        // 바이러스 위치 찾기
+        if (copy[nx][ny] != 0)
+            return false;
+
+        return true;
+    }
+
+    private static int[][] spread(int[][] copy) {
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        // 큐에 현재 바이러스 위치 입력
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (map[i][j] == 2) {
-                    queue.add(new Point(i, j));
-                }
+                if (copy[i][j] == 2)
+                    queue.offer(new int[] { i, j });
             }
         }
 
         while (!queue.isEmpty()) {
-            Point curr = queue.poll();
-
-            for (int k = 0; k < 4; k++) {
-                int nextX = curr.x + dx[k];
-                int nextY = curr.y + dy[k];
-
-                if (nextX >= 1 && nextY >= 1 && nextX <= n && nextY <= m) {
-                    if (copy[nextX][nextY] == 0) {
-                        copy[nextX][nextY] = 2;
-                        queue.add(new Point(nextX, nextY));
-                    }
+            int[] arr = queue.poll();
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = arr[0] + dx[dir];
+                int ny = arr[1] + dy[dir];
+                if (canGo(nx, ny, copy)) {
+                    copy[nx][ny] = 2;
+                    queue.offer(new int[] { nx, ny });
                 }
             }
         }
 
-        // 안전구역 확인
-        int sum = 0;
+        return copy;
+    }
+
+    private static void checkSafetyArea(int[][] copy) {
+        int result = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (copy[i][j] == 0) sum++;
+                if (copy[i][j] == 0)
+                    result += 1;
             }
         }
+        answer = Math.max(answer, result);
+    }
 
-        answer = Math.max(answer, sum);
+    private static int[][] copyMap() {
+        int[][] copy = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            copy[i] = map[i].clone();
+        }
+        return copy;
+    }
+
+    private static void dfs(int cnt) {
+
+        // 벽이 3개 세워졌다면
+        if (cnt == 3) {
+
+            // 배열 복사
+            int[][] copy = copyMap();
+
+            // 바이러스 퍼트리기
+            copy = spread(copy);
+
+            // 안전구역 체크
+            checkSafetyArea(copy);
+
+            return;
+        }
+
+        // 벽세우기
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // 벽이거나 바이러스라면 패스
+                if (map[i][j] == 1 || map[i][j] == 2)
+                    continue;
+                map[i][j] = 1;
+                dfs(cnt + 1);
+
+                // 백트래킹
+                map[i][j] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        map = new int[n + 1][m + 1];
+
+        // 연구실 초기 형태
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+        dfs(0);
+        System.out.println(answer);
     }
 }
