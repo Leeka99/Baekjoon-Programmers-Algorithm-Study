6fb87328cb19c5c8ac86ef080d5b45ad9227fae7
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-04-11 11:06:52 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2712 ms, Memory: 365756 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,37 +1,38 @@
 import java.util.*;
 
 public class Main {
-    // 도시개수, 도로개수,거리정보,시작위치
+    private static ArrayList<Integer>[] map;
+    private static boolean[] visited;
     private static int n, m, k, x;
-    private static ArrayList<Integer>[] graph;
-    private static int[] distance;
+    private static ArrayList<Integer> answer = new ArrayList<>();
 
-    private static void bfs() {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(x);
-        distance[x] = 0;
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            for (int next : graph[curr]) {
-                if (distance[next] == -1) {
-                    distance[next] = distance[curr] + 1;
-                    queue.offer(next);
-                }
+    private static void test() {
+        for (int i = 1; i < map.length; i++) {
+            System.out.print(i + ": ");
+            for (int j = 0; j < map[i].size(); j++) {
+                System.out.print(map[i].get(j) + ", ");
             }
+            System.out.println();
         }
     }
 
-    private static void answer() {
-        boolean found = false;
-        for (int i = 1; i <= n; i++) {
-            if (distance[i] == k) {
-                System.out.println(i);
-                found = true;
+    private static void bfs() {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] { x, 0 });
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            if (visited[curr[0]] == true)
+                continue;
+            for (int i = 0; i < map[curr[0]].size(); i++) {
+                queue.offer(new int[] { map[curr[0]].get(i), curr[1] + 1 });
             }
+            visited[curr[0]] = true;
+            if (curr[1] == k)
+                answer.add(curr[0]);
+            if (curr[1] > k)
+                break;
         }
-        if (!found)
-            System.out.println(-1);
     }
 
     public static void main(String[] args) {
@@ -40,23 +41,27 @@ public class Main {
         m = sc.nextInt();
         k = sc.nextInt();
         x = sc.nextInt();
-        graph = new ArrayList[n + 1];
 
-        for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+        map = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            map[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            graph[a].add(b);
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            map[num1].add(num2);
         }
         sc.close();
-
-        distance = new int[n + 1];
-        Arrays.fill(distance, -1);
-
         bfs();
-        answer();
-
+        Collections.sort(answer);
+        if (answer.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            for (int i = 0; i < answer.size(); i++) {
+                System.out.println(answer.get(i));
+            }
+        }
     }
 }
