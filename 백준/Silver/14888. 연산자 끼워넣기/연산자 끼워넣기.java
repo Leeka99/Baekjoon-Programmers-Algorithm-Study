961e746d56aae193bec7cdbd779eb57ef8f8cbe1
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-04-18 11:43:32 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 176 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,49 +1,54 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int n, answerMax = Integer.MIN_VALUE, answerMin = Integer.MAX_VALUE;
-    public static int[] numbers;
-    public static int[] toolsNum = new int[4];
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        numbers = new int[n];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
+    private static int n;
+    private static int maxNum = Integer.MIN_VALUE, minNum = Integer.MAX_VALUE;
+    private static int[] numArr;
+    // +, -, *, /
+    private static int[] operatorArr = new int[4];
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
-            toolsNum[i] = Integer.parseInt(st.nextToken());
-        }
+    private static void dfs(int result, int cnt) {
 
-        dfs(1,numbers[0]);
-
-        System.out.println(answerMax);
-        System.out.println(answerMin);
-
-    }
-    public static void dfs(int idx, int sum) {
-        if (idx == n) {
-            answerMax = Math.max(answerMax, sum);
-            answerMin = Math.min(answerMin, sum);
+        if (cnt == n) {
+            maxNum = Math.max(result, maxNum);
+            minNum = Math.min(result, minNum);
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-            if (toolsNum[i] > 0) {
-                toolsNum[i]--;
-
-                if (i == 0) dfs(idx + 1, sum + numbers[idx]);
-                if (i == 1) dfs(idx + 1, sum - numbers[idx]);
-                if (i == 2) dfs(idx + 1, sum * numbers[idx]);
-                if (i == 3) dfs(idx + 1, sum / numbers[idx]);
-
-                toolsNum[i]++;
+            if (operatorArr[i] != 0) {
+                operatorArr[i]--;
+                if (i == 0)
+                    dfs(result + numArr[cnt], cnt + 1);
+                if (i == 1)
+                    dfs(result - numArr[cnt], cnt + 1);
+                if (i == 2)
+                    dfs(result * numArr[cnt], cnt + 1);
+                if (i == 3)
+                    dfs(result / numArr[cnt], cnt + 1);
+                operatorArr[i]++;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        numArr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numArr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            operatorArr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        dfs(numArr[0], 1);
+        System.out.println(maxNum);
+        System.out.println(minNum);
     }
 }
