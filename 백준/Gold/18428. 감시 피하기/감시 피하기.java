8b8fe4dcdc60ef83001c9b2b97a13c25e66d05cf
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-04-20 11:41:05 +0900

    [Gold V] Title: 감시 피하기, Time: 184 ms, Memory: 18232 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.java
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -1,9 +1,27 @@
 import java.util.*;
 
+class Teacher {
+    private int x;
+    private int y;
+
+    public Teacher(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}
+
 public class Main {
     private static int n;
     private static char[][] map;
-    private static ArrayList<int[]> teacher = new ArrayList<>();
+    private static ArrayList<Teacher> t = new ArrayList<>();
     private static int[] dx = { -1, 0, 1, 0 };
     private static int[] dy = { 0, 1, 0, -1 };
 
@@ -21,48 +39,47 @@ public class Main {
         return 0 <= nx && 0 <= ny && nx < n && ny < n;
     }
 
-    private static boolean watch(int x, int y, int dir) {
-        while (true) {
-            x += dx[dir];
-            y += dy[dir];
-            if (!canGo(x, y))
-                return true;
-            if (map[x][y] == 'O')
-                return true;
-            if (map[x][y] == 'S')
-                return false;
-        }
-    }
-
     private static boolean findStudent() {
-        for (int i = 0; i < teacher.size(); i++) {
-            int x = teacher.get(i)[0];
-            int y = teacher.get(i)[1];
 
+        for (Teacher teacher : t) {
             for (int dir = 0; dir < 4; dir++) {
-                if (!watch(x, y, dir)) {
-                    return false;
+                int x = teacher.getX();
+                int y = teacher.getY();
+                while (true) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+                    if (!canGo(nx, ny))
+                        break;
+                    if (map[nx][ny] == 'S')
+                        return false;
+                    if (map[nx][ny] == 'O')
+                        break;
+
+                    x = nx;
+                    y = ny;
                 }
+
             }
         }
         return true;
     }
 
     private static boolean dfs(int cnt) {
-        // 벽 3개 세워졌다면 학생 보이는지 찾기
-        if (cnt == 3)
+        // 벽 3개 세워졌다면 탐색
+        if (cnt == 3) {
             return findStudent();
+        }
 
-        // 백트래킹
+        // 기둥 세우기, 백트래킹
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (map[i][j] != 'T' && map[i][j] != 'S' && map[i][j] != 'O') {
+                if (map[i][j] == 'X') {
                     map[i][j] = 'O';
-                    if (dfs(cnt + 1))
+                    boolean bool = dfs(cnt + 1);
+                    if (bool)
                         return true;
                     map[i][j] = 'X';
                 }
-
             }
         }
         return false;
@@ -72,23 +89,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         map = new char[n][n];
-        sc.nextLine();
 
+        sc.nextLine();
         for (int i = 0; i < n; i++) {
             String s = sc.nextLine().replace(" ", "");
             for (int j = 0; j < n; j++) {
                 char c = s.charAt(j);
-                if (c == 'T') {
-                    teacher.add(new int[] { i, j });
-                }
-                map[i][j] = c;
+                // 선생님 위치 저장
+                if (c == 'T')
+                    t.add(new Teacher(i, j));
+                map[i][j] = s.charAt(j);
             }
         }
         sc.close();
-        // test();
 
-        // 벽세우기 + 학생 보이는지 확인
-        boolean bool = dfs(0);
+        boolean bool = false;
+        bool = dfs(0);
+
         if (bool)
             System.out.println("YES");
         else
