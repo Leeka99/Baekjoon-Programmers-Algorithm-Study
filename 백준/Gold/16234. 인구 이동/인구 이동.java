0651a84d08c6d209b1b38f1f7b1e2eafee33ee6e
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-04-21 11:50:44 +0900

    [Gold IV] Title: 인구 이동, Time: 904 ms, Memory: 306092 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class Main {
-    private static int n, l, r, answer = 0;
+
+    private static int n, l, r, day = 0;
     private static int[][] map;
     private static boolean[][] visited;
     private static int[] dx = { -1, 0, 1, 0 };
@@ -11,41 +12,44 @@ public class Main {
         return 0 <= nx && 0 <= ny && nx < n && ny < n;
     }
 
-    private static boolean minus(int x1, int x2) {
-        int num = Math.abs(x1 - x2);
-        return l <= num && num <= r;
-    }
-
-    private static List<int[]> bfs(int x, int y) {
+    private static boolean bfs(int i, int j) {
+        ArrayList<int[]> check = new ArrayList<>();
         Queue<int[]> queue = new ArrayDeque<>();
-        List<int[]> temp = new ArrayList<>();
-        queue.offer(new int[] { x, y });
-        temp.add(new int[] { x, y });
-        visited[x][y] = true;
-        int sum = map[x][y];
+        queue.offer(new int[] { i, j });
+        int sum = map[i][j], cnt = 1;
+        visited[i][j] = true;
+        check.add(new int[] { i, j });
 
         while (!queue.isEmpty()) {
             int[] curr = queue.poll();
+            int x = curr[0];
+            int y = curr[1];
             for (int dir = 0; dir < 4; dir++) {
-                int nx = curr[0] + dx[dir];
-                int ny = curr[1] + dy[dir];
-                if (!canGo(nx, ny) || visited[nx][ny])
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (!canGo(nx, ny))
                     continue;
-                if (minus(map[curr[0]][curr[1]], map[nx][ny])) {
-                    visited[nx][ny] = true;
+                if (visited[nx][ny])
+                    continue;
+                int temp = Math.abs(map[x][y] - map[nx][ny]);
+                if (l <= temp && temp <= r) {
                     queue.offer(new int[] { nx, ny });
-                    temp.add(new int[] { nx, ny });
+                    visited[nx][ny] = true;
+                    check.add(new int[] { nx, ny });
                     sum += map[nx][ny];
+                    cnt += 1;
                 }
             }
         }
-        int avg = sum / temp.size();
-
-        for (int[] p : temp) {
-            map[p[0]][p[1]] = avg;
-        }
-
-        return temp;
+        if (sum > map[i][j]) {
+            // 연합 진행
+            int amount = sum / cnt;
+            for (int[] p : check) {
+                map[p[0]][p[1]] = amount;
+            }
+            return true;
+        } else
+            return false;
     }
 
     private static void test() {
@@ -63,39 +67,32 @@ public class Main {
         n = sc.nextInt();
         l = sc.nextInt();
         r = sc.nextInt();
-
         map = new int[n][n];
+        // s = 50*50
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 map[i][j] = sc.nextInt();
             }
         }
         sc.close();
-        
-        // 인구이동
+
         while (true) {
             visited = new boolean[n][n];
-            boolean moved = false;
+            boolean bool = false;
 
+            // s = 50 * 50
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (!visited[i][j]) {
-                        List<int[]> temp = bfs(i, j);
-                        if (temp.size() > 1) {
-                            moved = true;
-                        }
+                    if (!visited[i][j] && bfs(i, j)) {
+                        bool = true;
                     }
                 }
             }
-
-            // 연합 없었다면 정지
-            if (!moved) {
-                break;
-            }
-            answer++;
+            if (!bool) {
+                System.out.println(day);
+                return;
+            } else
+                day += 1;
         }
-
-        // 최종 답
-        System.out.println(answer);
     }
 }
