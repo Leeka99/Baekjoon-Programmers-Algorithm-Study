b29d46bb668e4ff98efda505e32e2e8e96e1c80e
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-09-10 10:55:04 +0900

    [level 2] Title: 프로세스, Time: 3.58 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,40 +1,45 @@
+
+// 9시50분
+// 규칙 따라 몇번째 실행?
+//순서
+// 1. 큐에서 하나 꺼내기
+// 2. 우선순위 큰거 더 있으면 큐에 넣기
+// 3. 꺼낸게 제일 높다면 꺼낸거 실행
+// * 실행하면 버리기, 같다면 뒤에가 먼저
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        Queue<Integer> queue2 = new LinkedList<>();
-        List<Integer> r = new ArrayList<>();
-        
-        for (int index = 0; index < priorities.length; index++) {
-            queue.add(priorities[index]);
-            queue2.add(index);
-            r.add(priorities[index]);
+        int answer = 1;
+        Queue<Integer> original = new ArrayDeque<>();
+        Queue<Integer> index = new ArrayDeque<>();
+        Queue<Integer> target = new ArrayDeque<>();
+        for (int i = 0; i < priorities.length; i++) {
+            original.offer(priorities[i]);
+            index.offer(i);
+        }
+        Arrays.sort(priorities);
+        for (int i = priorities.length - 1; i >= 0; i--) {
+            target.offer(priorities[i]);
         }
         
-        r.sort((a,b) -> Integer.compare(b, a));
-                
-        int rank = 0;
-        while(!queue.isEmpty()) {
-            int number = queue.poll();
-            int index = queue2.poll();
-            if (r.get(rank) != number) {
-                queue.add(number);
-                queue2.add(index);
-                continue;
+        while(!original.isEmpty()) {
+            int targetNum = target.peek();
+            int originalNum = original.peek();
+            int indexNum = index.peek();
+            if (targetNum == originalNum){
+                if (indexNum == location) break;
+                else {
+                    target.poll();
+                    original.poll();
+                    index.poll();
+                    answer++;
+                }
             }
-            test(rank, number);
-            if (index == location) {
-                answer = rank + 1;
-                break;
+            else {
+                original.offer(original.poll());
+                index.offer(index.poll());
             }
-            rank++;
         }
-        
         return answer;
-    }
-    
-    public void test(int rank, int num) {
-        System.out.println("rank: " + rank + ", num: " + num);
     }
 }
