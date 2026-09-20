78abbc631f6d539027ef6fe96b0320c7ff480eea
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-09-20 21:19:07 +0900

    [level 2] Title: 타겟 넘버, Time: 4.54 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,23 @@
 import java.util.*;
 class Solution {
-    private static int answer = 0;
-    private static void dfs(int[] numbers, int target, int sum, int index) {
+    public int solution(int[] numbers, int target) {
+        
+        return dfs(numbers, target, 0, 0);
+    }
+    
+    public int dfs(int[] numbers, int target, int ad, int index) {
+        int cnt = 0;
         
         if (index == numbers.length) {
-            if (sum == target) answer++;
-            return;
+            if (ad == target) {
+               cnt += 1;
+            }
+             return cnt;
         }
         
-        dfs(numbers, target, sum+numbers[index], index+1);
-        dfs(numbers, target, sum-numbers[index], index+1);
+        cnt += dfs(numbers, target, ad + numbers[index], index + 1);
+        cnt += dfs(numbers, target, ad - numbers[index], index + 1);
         
-    }
-    public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0,0);
-        
-        return answer;
+        return cnt;
     }
 }
