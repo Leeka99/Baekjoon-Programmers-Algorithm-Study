b89b089931d5370ec6504b2a96a387d953be9463
Kanghyeon Lee <139203708+Leeka99@users.noreply.github.com>
2026-09-24 10:59:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.56 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,69 +1,30 @@
 import java.util.*;
-
-class Pair {
-    int x, y;
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 class Solution {
-    private static int n, m; 
-    private static int[][] maps;
-    private static int[] dx = new int[]{0, 0, 1, -1};
-    private static int[] dy = new int[]{-1, 1, 0, 0};
-    private static Queue<Pair> queue = new LinkedList<>(); 
-    
-    private static boolean inRange(int x, int y) {
-        return 0 <= x && x < n && 0 <= y && y < m;
-    }
-    
-    private static boolean canGo(int x, int y) {
-        return inRange(x, y) && maps[x][y] == 1;
-    }
-    
-    private static void push(int x, int y) {
-        queue.add(new Pair(x, y));
-    }
-    
-    private static int bfs() {
-        int[][] dist = new int[n][m]; // 거리 배열
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], -1);
-        }
-        
-        dist[0][0] = 1;
-        push(0, 0);
+    public int solution(int[][] maps) {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0,0,1});
+        visited[0][0] = true;
         
         while(!queue.isEmpty()) {
-            Pair curr = queue.poll();
-            int x = curr.x;
-            int y = curr.y;
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if (canGo(nx, ny) && dist[nx][ny] == -1) {
-                    dist[nx][ny] = dist[x][y] + 1; 
-                    push(nx, ny);
-                    
-                    if (nx == n - 1 && ny == m - 1) {
-                        return dist[nx][ny];
-                    }
-                }
+            int[] curr = queue.poll();
+            if (curr[0] == maps.length - 1 && curr[1] == maps[0].length - 1) {             
+                return curr[2];
+            }
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = curr[0] + dx[dir];
+                int ny = curr[1] + dy[dir];
+                if (!canGo(nx, ny, maps) || maps[nx][ny] == 0 || visited[nx][ny]) continue;
+                visited[nx][ny] = true;
+                queue.offer(new int[]{nx, ny, curr[2] + 1});
             }
         }
         
         return -1;
     }
-        
-    public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-        this.maps = maps;
-        
-        return bfs();
+    public boolean canGo(int nx, int ny, int[][] maps) {
+        return 0 <= nx && 0 <= ny && nx < maps.length && ny < maps[0].length;
     }
 }
